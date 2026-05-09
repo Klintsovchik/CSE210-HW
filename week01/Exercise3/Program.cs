f6f851cd627dd7679d9bb6f4ff48a84022ec20a1
klintsovchik <timothyklintsov@gmail.com>
2026-05-09 15:58:22 +0400

    Finished 3rd Exercise with enhancment

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        string playAgain = "yes";
+        while (playAgain.ToLower() == "yes")
+        {
+            Console.Write("Give the magical number: ");
+            string magicalNumber = Console.ReadLine();
+            int magicalNumberInt = int.Parse(magicalNumber);
+            int guessCount = 0;
+            int guessInt = -1;
+
+            while (guessInt != magicalNumberInt)
+            {
+                Console.Write("Guess the magical number: ");
+                string guess = Console.ReadLine();
+                guessInt = int.Parse(guess);
+                guessCount++;
+                if (guessInt < magicalNumberInt)
+                {
+                    Console.WriteLine("Higher!");
+                }
+                else if (guessInt > magicalNumberInt)
+                {
+                    Console.WriteLine("Lower!");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                }
+            }
+
+            Console.WriteLine($"It took you {guessCount} guesses.");
+
+            Console.Write("Do you want to play again? (yes/no) ");
+            playAgain = Console.ReadLine();
+        }
+        Console.WriteLine("Thanks for playing!");
     }
 }
