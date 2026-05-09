d50cf10e03bd2d0bd6028d6d3d29d4ec3429f1fc
klintsovchik <timothyklintsov@gmail.com>
2026-05-09 15:13:03 +0400

    Second exercise is done

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,65 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is your grade percentage? ");
+        string input = Console.ReadLine();
+        int gradePercentage = int.Parse(input);
+        string letterGrade;
+        
+            if (gradePercentage >= 90)
+            {
+                letterGrade = "A";
+            }
+            else if (gradePercentage >= 80)
+            {
+                letterGrade = "B";
+            }
+            else if (gradePercentage >= 70)
+            {
+                letterGrade ="C";
+            }
+            else if (gradePercentage >= 60)
+            {
+                letterGrade = "D";
+            }
+            else
+            {
+                letterGrade = "F";
+            }
+            int lastDigit = gradePercentage % 10;
+            string sign;
+            if (lastDigit >= 7)
+            {
+                sign = "+";
+            }
+            else if (lastDigit <= 3)
+            {
+                sign = "-";
+            }
+            else
+            {
+                sign = "";
+            }
+
+            if (letterGrade != "F")
+            {
+                letterGrade += sign;
+            }
+            if (letterGrade == "A" && sign == "+")
+            {
+                letterGrade = "A";
+            }
+            Console.WriteLine($"Your letter grade is {letterGrade}");
+            
+            if (gradePercentage >= 70)
+            {
+                Console.WriteLine("Congratulations! You passed the class!");
+            }
+            else
+            {
+                Console.WriteLine("Sorry, you did not pass the class. Better luck next time!");
+            }
+        }
+        
+        
     }
-}
