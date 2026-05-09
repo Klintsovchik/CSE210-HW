971988e9c46679526c4cc5e0adb9030eaca2674b
klintsovchik <timothyklintsov@gmail.com>
2026-05-09 19:11:40 +0400

    Exercise number 4 is completed, with stretch Challenge

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -4,6 +4,54 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        Console.Write("Write series of numbers and stop with a 0: ");
+        string numbers = Console.ReadLine();
+        List<int> numberList = new List<int>();
+        while (numbers != "0")
+        {
+            int number = int.Parse(numbers);
+            numberList.Add(number);
+            Console.Write("Write series of numbers and stop with a 0: ");
+            numbers = Console.ReadLine();
+        }
+        int sum = 0;
+        foreach (int number in numberList)        {
+            sum += number;
+        }
+        Console.WriteLine($"The sum of the numbers is: {sum}");
+        float average = sum / numberList.Count;
+        Console.WriteLine($"The average of the numbers is: {average:F2}");
+        int max = numberList[0];
+        foreach (int number in numberList)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+        Console.WriteLine($"The maximum number is: {max}");
+        int minPositive = int.MaxValue;
+        foreach (int number in numberList)
+        {
+            if (number > 0 && number < minPositive)
+            {
+                minPositive = number;
+            }
+        }
+        if (minPositive == int.MaxValue)
+        {
+            Console.WriteLine("There are no positive numbers.");
+        }
+        else
+        {
+            Console.WriteLine($"The minimum positive number is: {minPositive}");
+        }
+        numberList.Sort();
+        Console.WriteLine("The numbers in sorted order are:");
+        foreach (int number in numberList)
+        {
+            Console.Write($"{number} ");
+        }
+        Console.WriteLine();
     }
 }
