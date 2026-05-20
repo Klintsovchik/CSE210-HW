3bb32f36186b814a58d3cfeee93873f97d77271b
klintsovchik <timothyklintsov@gmail.com>
2026-05-20 12:22:58 +0400

    Fraction program

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -1,9 +1,24 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        Fraction f = new Fraction();
+        Console.WriteLine(f);
+
+        Fraction d = new Fraction(5);
+        Console.WriteLine(d);
+
+        Fraction s =new Fraction(6, 7);
+        Console.WriteLine(s);
+
+        f.Top = 10;
+        f.Bottom = 12;
+        Console.WriteLine(f.Top);
+        Console.WriteLine(f.Bottom);
+        Console.WriteLine(f.GetFractionString());
+        Console.WriteLine(f.GetDecimalValue());
     }
 }
